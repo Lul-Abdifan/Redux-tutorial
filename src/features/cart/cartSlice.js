5076dd9563dcd8d773b23fddb3e9445fd419834a
Lul-Abdifan <tasanbannu@gmail.com>
2023-05-09 10:46:42 -0400

    Add components and redux

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
+const initialState = {
+  cartItems: cartItems,
+  amount: 10,
+  total: 0,
+  isLoading: true,
+};
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    increase: (state, action) => {
+      const cartId =action.payload.id;
+      const cartItem = state.cartItems.find((item) => item.id === cartId);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = total;
+    },
+  },
+});
+export const { removeItem,clearCart, increase, decrease,calculateTotals } = cartSlice.actions;
+export default cartSlice.reducer;
